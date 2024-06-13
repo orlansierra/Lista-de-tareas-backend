d66518d0309ed0709075d776add55e3cab6b2c21
orlansierra <sierraorlan25@gmail.com>
2024-06-12 22:18:39 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    commonjs: true,
+  },
+  globals: {
+    process: true,
+    'jest/globals': true,
+  },
+  plugins: ['jest'],
+  extends: ['eslint:recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+  },
+};
